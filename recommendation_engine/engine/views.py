50ddccb85919686fe82c72582245ca75c7dc74d6
Praful <prafulkrsingh1@gmail.com>
2016-07-14 16:29:56 +0530

    more methods added

--- recommendation_engine/engine/views.py
+++ recommendation_engine/engine/views.py
@@ -7,7 +7,15 @@ def index(request):
 	return HttpResponse("Hello World!")
 
 def show(request, book_id):
+	# Update clicks table
 	return HttpResponse(book_id)
 
-def 
+def buy_now(request, book_id):
+	# update 
+	return HttpResponse(book_id)
+
+def product_rating(request, book_id):
+
+	return HttpResponse(book_id)
+
 	
