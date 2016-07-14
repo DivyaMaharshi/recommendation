5a5bd767fbb08330c30e0085b15bad4002c38dfc
Praful <prafulkrsingh1@gmail.com>
2016-07-14 16:25:13 +0530

    urls and views added

--- recommendation_engine/engine/views.py
+++ recommendation_engine/engine/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+	return HttpResponse("Hello World!")
+
+def show(request, book_id):
+	return HttpResponse(book_id)
+
+def 
+	
