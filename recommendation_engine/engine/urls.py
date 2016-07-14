367ff2f7f4ae913bc633273a24c57f793ae058c6
Praful <prafulkrsingh1@gmail.com>
2016-07-14 18:48:40 +0530

    views created

--- recommendation_engine/engine/urls.py
+++ recommendation_engine/engine/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<book_id>[0-9]+)/$', views.show, name='show'),
+    url(r'^(?P<book_id>[0-9]+)/buy$', views.buy_now, name='buy_now'),
+    url(r'^(?P<book_id>[0-9]+)/rating$', views.product_rating, name='product_rating'),
 ]
--- recommendation_engine/engine/views.py
+++ recommendation_engine/engine/views.py
@@ -1,21 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Users, Books
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello World!")
+	books = Books.objects.all()
+	return render(request, 'engine/index.html', {'books': books})
 
 def show(request, book_id):
+	user_id = 1
 	# Update user_click_history
-	return HttpResponse(book_id)
+	try:
+		x = UserClickHistory.objects.get(user_id=user_id,book_id=book_id)
+	except UserClickHistory.DoesNotExist
+		x = UserClickHistory.objects.create(user_id=user_id, book_id=book_id, clicks=0)
+	x.clicks = x.clicks + 1
+	x.save
+
+	book = Books.objects.get(pk=book_id)
+	return render(request, 'engine/show.html',{'book': book})
 
 def buy_now(request, book_id):
-	# update purchase_history and user_bought_history
-	return HttpResponse(book_id)
+	user_id = 1
+	# update purchase_history
+	try:
+		x = PurchaseHistory.objects.get(book_id=book_id)
+	except PurchaseHistory.DoesNotExist:
+		x = PurchaseHistory.objects.create(book_id=book_id, quantity=0, rating=0, rating_count=0)
+	x.quantity = x.quantity + 1
+	x.save
+
+	# update user_bought_history
+	try:
+		ub = UserBoughtHistory.objects.get(user_id=user_id, book_id=book_id)
+	except UserBoughtHistory.DoesNotExist:
+		ub = UserBoughtHistory.objects.create(book_id=book_id, user_id=user_id)
+
+	book = Books.objects.get(pk=book_id)
+	return render(request, 'engine/rating.html', {'book': book, 'loop_times': range(1,6)})
 
 def product_rating(request, book_id):
+	user_id = 1
+	rating = request.POST.get("user_rating")
 	#update purchase_history
-	return HttpResponse(book_id)
+	try:
+		x = PurchaseHistory.objects.get(book_id=book_id)
+	except PurchaseHistory.DoesNotExist:
+		x = PurchaseHistory.objects.create(book_id=book_id, quantity=0, rating=0, rating_count=0)
+	x.rating = (x.rating*x.rating_count + rating)/(x.rating_count + 1)
+	x.rating_count = x.rating_count + 1
+	x.save
+	
+	return HttpResponse("You rated this book " + rating + " star")
+
 
 	
